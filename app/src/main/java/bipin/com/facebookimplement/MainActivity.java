ee29552449ccc3f2b4c080aa940715edfea460ee
bipinkumar <bipin@threesquare.in>
2016-05-17 18:23:41 +0530

    facebook integration and login

---
+++ app/src/main/java/bipin/com/facebookimplement/MainActivity.java
@@ -0,0 +1,171 @@
+package bipin.com.facebookimplement;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class MainActivity extends AppCompatActivity {
+    CallbackManager callbackManager;
+    Button share,details;
+    ShareDialog shareDialog;
+    LoginButton login;
+    ProfilePictureView profile;
+    Dialog details_dialog;
+    TextView details_txt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        generateHashKey();
+        callbackManager = CallbackManager.Factory.create();
+        login = (LoginButton)findViewById(R.id.login_button);
+        profile = (ProfilePictureView)findViewById(R.id.picture);
+        shareDialog = new ShareDialog(this);
+        share = (Button)findViewById(R.id.share);
+        details = (Button)findViewById(R.id.details);
+        login.setReadPermissions("public_profile email");
+        share.setVisibility(View.INVISIBLE);
+        details.setVisibility(View.INVISIBLE);
+        details_dialog = new Dialog(this);
+        details_dialog.setContentView(R.layout.dialog_layout);
+        details_dialog.setTitle("Details");
+        details_txt = (TextView)details_dialog.findViewById(R.id.details);
+        details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                details_dialog.show();
+            }
+        });
+
+
+        if(AccessToken.getCurrentAccessToken() != null){
+            RequestData();
+            share.setVisibility(View.VISIBLE);
+            details.setVisibility(View.VISIBLE);
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(AccessToken.getCurrentAccessToken() != null) {
+                    share.setVisibility(View.INVISIBLE);
+                    details.setVisibility(View.INVISIBLE);
+                    profile.setProfileId(null);
+                }
+            }
+        });
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShareLinkContent content = new ShareLinkContent.Builder().build();
+                shareDialog.show(content);
+
+            }
+        });
+        login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                if(AccessToken.getCurrentAccessToken() != null){
+                    RequestData();
+                    share.setVisibility(View.VISIBLE);
+                    details.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+            }
+        });
+
+    }
+
+    private void generateHashKey() {
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "bipin.com.facebookimplement",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:",
+                        Base64.encodeToString(md.digest(),
+                        Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+
+    public void RequestData(){
+        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object,GraphResponse response) {
+
+                JSONObject json = response.getJSONObject();
+                try {
+                    if(json != null){
+                        String text = "<b>Name :</b> "+json.getString("name")+"<br><br><b>Email :</b> "+json.getString("email")+"<br><br><b>Profile link :</b> "+json.getString("link");
+                        details_txt.setText(Html.fromHtml(text));
+                        profile.setProfileId(json.getString("id"));
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,link,email,picture");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
